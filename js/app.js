54257ec36da8cbe9d9238eb69120cf13c9a89bdb
Isaiah Nunez <INunez@ucvts.tec.nj.us>
2020-02-04 12:46:37 -0500

    sixth

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@
 
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 let board;s
+let turn;
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
-
+const squares = Array.from(document.querySelectorAll("#board div"));
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 window.onload = init;
 ///////////////////// FUNCTIONS /////////////////////////////////////
@@ -13,9 +14,13 @@ function init() {
     "", "", "",
     "", "", ""
   ];
+  turn = "X";
+
+  render();
+}
 
   function render() {
     board.forEach(function(mark, index) {
-      console.log(mark, index);
+      squares[index].textContent = mark;    // writes an X or an O on board
     });
   }
